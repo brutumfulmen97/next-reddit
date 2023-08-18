bd3c36cd170c623c29492ad3afac695de6fb7e89
brutumfulmen97 <vlatkooop@gmail.com>
2023-08-18 18:22:59 +0200

    dropdown menu u navu

---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,36 @@
+import { Avatar, AvatarProps, AvatarFallback } from "@radix-ui/react-avatar";
+import { Divide } from "lucide-react";
+import { User } from "next-auth";
+import Image from "next/image";
+import { FC } from "react";
+import { Icons } from "./Icons";
+
+interface UserAvatarProps extends AvatarProps {
+    user: Pick<User, "name" | "image">;
+}
+
+const UserAvatar: FC<UserAvatarProps> = ({ user, ...props }) => {
+    return (
+        <Avatar {...props}>
+            {user.image ? (
+                <div className="relative aspect-square h-full w-full">
+                    <Image
+                        width={40}
+                        height={40}
+                        src={user.image}
+                        alt="profile picture"
+                        referrerPolicy="no-referrer"
+                        className="rounded-full object-cover"
+                    />
+                </div>
+            ) : (
+                <AvatarFallback className="sr-only">
+                    {user?.name}
+                    <Icons.user className="h-4 w-4" />
+                </AvatarFallback>
+            )}
+        </Avatar>
+    );
+};
+
+export default UserAvatar;
