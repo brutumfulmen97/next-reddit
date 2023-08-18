2c8f796380cb1731361fbd9b627892541ebfeeb2
brutumfulmen97 <vlatkooop@gmail.com>
2023-08-18 17:16:53 +0200

    auth gotov

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { FC, useState } from "react";
-import { Button } from "./ui/Button";
 import { signIn } from "next-auth/react";
-import { Icons } from "./Icons";
+import * as React from "react";
+import { FC } from "react";
+import { Button } from "@/components/ui/Button";
 import { useToast } from "@/hooks/use-toast";
+import { Icons } from "./Icons";
+
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
-    const [isLoading, setIsLoading] = useState<boolean>(false);
     const { toast } = useToast();
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
     const loginWithGoogle = async () => {
         setIsLoading(true);
@@ -19,23 +21,26 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
             await signIn("google");
         } catch (error) {
             toast({
-                title: "There was problem.",
-                description: "There was an error logging in with Google.",
+                title: "Error",
+                description: "There was an error logging in with Google",
                 variant: "destructive",
             });
         } finally {
             setIsLoading(false);
         }
     };
+
     return (
         <div className={cn("flex justify-center", className)} {...props}>
             <Button
                 isLoading={isLoading}
+                type="button"
                 size="sm"
                 className="w-full"
                 onClick={loginWithGoogle}
+                disabled={isLoading}
             >
-                {isLoading ? null : <Icons.google className="w-4 h-4 mr-2" />}
+                {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
                 Google
             </Button>
         </div>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,8 @@
-import { NextAuthOptions } from "next-auth";
+import { db } from "@/lib/db";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { db } from "./db";
-import GoogleProvider from "next-auth/providers/google";
 import { nanoid } from "nanoid";
+import { NextAuthOptions, getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(db),
@@ -30,16 +30,19 @@ export const authOptions: NextAuthOptions = {
 
             return session;
         },
+
         async jwt({ token, user }) {
             const dbUser = await db.user.findFirst({
                 where: {
                     email: token.email,
                 },
             });
+
             if (!dbUser) {
                 token.id = user!.id;
                 return token;
             }
+
             if (!dbUser.username) {
                 await db.user.update({
                     where: {
@@ -64,3 +67,5 @@ export const authOptions: NextAuthOptions = {
         },
     },
 };
+
+export const getAuthSession = () => getServerSession(authOptions);
