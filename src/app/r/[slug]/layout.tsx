769908431f5c7fb5c2967ab963ea7d688da25a3d
brutumfulmen97 <vlatkooop@gmail.com>
2023-08-20 15:26:28 +0200

    post creation form

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,18 +1,25 @@
 import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
-import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
+// import ToFeedButton from "@/components/ToFeedButton";
+import { buttonVariants } from "@/components/ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { format } from "date-fns";
+import type { Metadata } from "next";
+import Link from "next/link";
 import { notFound } from "next/navigation";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+    title: "Breadit",
+    description: "A Reddit clone built with Next.js and TypeScript.",
+};
 
 const Layout = async ({
     children,
     params: { slug },
 }: {
-    children: React.ReactNode;
-    params: {
-        slug: string;
-    };
+    children: ReactNode;
+    params: { slug: string };
 }) => {
     const session = await getAuthSession();
 
@@ -43,9 +50,7 @@ const Layout = async ({
 
     const isSubscribed = !!subscription;
 
-    if (!subreddit) {
-        return notFound();
-    }
+    if (!subreddit) return notFound();
 
     const memberCount = await db.subscription.count({
         where: {
@@ -56,24 +61,24 @@ const Layout = async ({
     });
 
     return (
-        <div className="sm:container max-w-7xl mx-atuo h-full pt-12">
+        <div className="sm:container max-w-7xl mx-auto h-full pt-12">
             <div>
-                {/* TODO:  Button to take us back */}
+                {/* <ToFeedButton /> */}
 
-                <div className="gird grid-cols-1 md:gird-cols-3 gap-y-4 md:gap-x-4 py-6">
-                    <div className="flex flex-col col-span-2 space-y-6">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
+                    <ul className="flex flex-col col-span-2 space-y-6">
                         {children}
+                    </ul>
 
-                        {/* info sidebar*/}
-                        <div className="hidden md:block overlow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
-                            <div className="px-6 py-4">
-                                <p className="font-semibold py-3">
-                                    About r/{subreddit.name}
-                                </p>
-                            </div>
+                    {/* info sidebar */}
+                    <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
+                        <div className="px-6 py-4">
+                            <p className="font-semibold py-3">
+                                About r/{subreddit.name}
+                            </p>
                         </div>
                         <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
-                            <div className="felx justify-between gap-x-4 py-4">
+                            <div className="flex justify-between gap-x-4 py-3">
                                 <dt className="text-gray-500">Created</dt>
                                 <dd className="text-gray-700">
                                     <time
@@ -88,27 +93,36 @@ const Layout = async ({
                             </div>
                             <div className="flex justify-between gap-x-4 py-3">
                                 <dt className="text-gray-500">Members</dt>
-                                <dd className="text-gray-700">
+                                <dd className="flex items-start gap-x-2">
                                     <div className="text-gray-900">
                                         {memberCount}
                                     </div>
                                 </dd>
                             </div>
-                            {subreddit.creatorId === session?.user.id ? (
+                            {subreddit.creatorId === session?.user?.id ? (
                                 <div className="flex justify-between gap-x-4 py-3">
-                                    <p className="text-gray-500">
-                                        You created this community.
-                                    </p>
+                                    <dt className="text-gray-500">
+                                        You created this community
+                                    </dt>
                                 </div>
                             ) : null}
 
-                            {subreddit.creatorId !== session?.user.id && (
+                            {subreddit.creatorId !== session?.user?.id ? (
                                 <SubscribeLeaveToggle
+                                    isSubscribed={isSubscribed}
                                     subredditId={subreddit.id}
                                     subredditName={subreddit.name}
-                                    isSubscribed={isSubscribed}
                                 />
-                            )}
+                            ) : null}
+                            <Link
+                                className={buttonVariants({
+                                    variant: "outline",
+                                    className: "w-full mb-6",
+                                })}
+                                href={`r/${slug}/submit`}
+                            >
+                                Create Post
+                            </Link>
                         </dl>
                     </div>
                 </div>
